0ee933b34f982cd21d78a86347a22da94d570a10
Sejalsavran <sejalsavran2004@gmail.com>
2023-08-05 09:23:28 +0530

    made change stuff

---
+++ function.java
@@ -0,0 +1,23 @@
+public class function {
+    //A method to calculate sum of two numbers
+
+    public static void sum(int a, int b) {
+        int sum = a+b;
+        System.out.println(sum);
+    }
+    public static void main(String args[]) {
+        int a =10;
+        int b = 20;
+        sum(a, b); //function call
+        product(a, b);//function call
+    }
+     //to calculate product of two number
+
+     public static void product(int a, int b) {
+        int product = a*b;
+        System.out.println(product);
+     }
+     
+
+     }
+    

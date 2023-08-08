571c08b0f2b662a2d53a38380d268913b1c73264
Sejalsavran <sejalsavran2004@gmail.com>
2023-08-08 10:00:37 +0530

    made changes stuff

--- BitMask.java
+++ BitMask.java
@@ -1,9 +1,11 @@
 
 public class BitMask {
     public static void main(String args[]) {
+
+      //get bit
         int n =5; 
         int pos =2;
-        int bitMask = 1<<pos;
+        int bitMask = 1<<pos;  //left shift by position times
 
         if((bitMask & n) == 0) {
           System.out.println("Bit was Zero");
@@ -11,7 +13,12 @@ public class BitMask {
           System.out.println("Bit was non-zero");
         }
 
-   
+         //set bit
+     pos =1;
+     bitMask = 1<<pos;
+
+     int newNumber = bitMask | n; 
+     System.out.println(newNumber);
         }
        
 }
